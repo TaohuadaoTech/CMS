37e1a56d4da1c4e6cfebd8092a69be2a9416334e
hys <hys@email.com>
2023-09-04 15:19:06 +0800

    补充核心文件

---
+++ application/extra/site.php
@@ -0,0 +1,59 @@
+<?php
+
+return array (
+    'name' => '桃花岛',
+    'beian' => '',
+    'cdnurl' => '',
+    'version' => '1.0.125',
+    'timezone' => 'Asia/Shanghai',
+    'forbiddenip' => '',
+    'languages' =>
+        array (
+            'backend' => 'zh-cn',
+            'frontend' => 'zh-cn',
+        ),
+    'fixedpage' => 'index/directions',
+    'categorytype' =>
+        array (
+            'default' => 'Default',
+            'page' => 'Page',
+            'article' => 'Article',
+            'test' => 'Test',
+        ),
+    'configgroup' =>
+        array (
+            'basic' => 'Basic',
+        ),
+    'mail_type' => '1',
+    'mail_smtp_host' => '',
+    'mail_smtp_port' => '',
+    'mail_smtp_user' => '',
+    'mail_smtp_pass' => '',
+    'mail_verify_type' => '1',
+    'mail_from' => '',
+    'attachmentcategory' =>
+        array (
+            'category1' => 'Category1',
+            'category2' => 'Category2',
+            'custom' => 'Custom',
+        ),
+    'mail_switch' => '0',
+    'prevent_gmail_alias_switch' => '0',
+    'mail_suffix_whitelist_switch' => '0',
+    'mail_suffix_whitelist' => '',
+    'anti_machine_switch' => '0',
+    'google_recaptcha_key' => '',
+    'google_recaptcha_site_key' => '',
+    'google_recaptcha_score' => '0.5',
+    'ip_registered_restriction' => '0',
+    'ip_max_registered_times' => '10',
+    'ip_punishment_time' => '2',
+    'system_notice' => '<p style="margin: 0px; text-rendering: optimizelegibility; font-feature-settings: \'kern\'; font-kerning: normal; font-family: PingFangSC-Regular, \'PingFang SC\', sans-serif; font-size: 16px;"><strong>欢迎使用桃花岛CMS建站系统，我们强烈建议您加入我们的群组，以便获取最佳技术支持服务！</strong></p>
+<p style="margin: 0px; text-rendering: optimizelegibility; font-feature-settings: \'kern\'; font-kerning: normal; font-family: PingFangSC-Regular, \'PingFang SC\', sans-serif; font-size: 16px;"><strong>Telegram：https://t.me/taohuadaoCMS</strong></p>
+<p style="margin: 0px; text-rendering: optimizelegibility; font-feature-settings: \'kern\'; font-kerning: normal; font-family: PingFangSC-Regular, \'PingFang SC\', sans-serif; font-size: 16px;"><strong>官方网站：https://www.taohuadao.org</strong></p>
+<p style="margin: 0px; text-rendering: optimizelegibility; font-feature-settings: \'kern\'; font-kerning: normal; font-family: PingFangSC-Regular, \'PingFang SC\', sans-serif; font-size: 16px;"><strong>官方论坛：https://bbs.taohuadao.net</strong><br /><strong>付费业务：https://www.taohuadao.net</strong></p>
+<p style="margin: 0px; text-rendering: optimizelegibility; font-feature-settings: \'kern\'; font-kerning: normal; font-family: PingFangSC-Regular, \'PingFang SC\', sans-serif; font-size: 16px;"><strong>官方github：https://github.com/TaohuadaoTech/CMS</strong></p>
+<p style="margin: 0px; text-rendering: optimizelegibility; font-feature-settings: \'kern\'; font-kerning: normal; font-family: PingFangSC-Regular, \'PingFang SC\', sans-serif; font-size: 16px;">&nbsp;</p>
+<p style="margin: 0px; text-rendering: optimizelegibility; font-feature-settings: \'kern\'; font-kerning: normal; font-family: PingFangSC-Regular, \'PingFang SC\', sans-serif; font-size: 16px;"><span style="color: #1f2328; font-family: -apple-system, \'system-ui\', \'Segoe UI\', \'Noto Sans\', Helvetica, Arial, sans-serif, \'Apple Color Emoji\', \'Segoe UI Emoji\'; background-color: #ffffff;">我们知道目前市面上有各种CMS系统，如苹果CMS、飞飞CMS以及传统的帝国CMS等等。他们都可以用来搭建影视网站，但是他们也都存在着各种问题，如系统臃肿、功能繁多，对于新手站长很不友好，代码分支很多、山寨埋有后门的版本在网络上流传，站长难以辨认。配置繁琐，如采集视频经常会遇到域名失效，需要操作数据库等，难以保证网站随时都能正常访问，一旦出现问题就会造成用户无法访问，用户流失等。所以我们才针对这些站长的痛点，推出了专属于影视建站系统领域的桃花岛CMS系统。</span></p>',
+    'skip_ads_after_seconds' => '5',
+);
